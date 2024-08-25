a0b3fce5c7c71919f08a5079ec926c8bf6a48e25
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-26 00:39:15 +0800

    feat: Add Hero component to Home page

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="flex flex-col justify-center items-center py-20 lg:py-20 md:py-20 sm:py-20">
+      <h1 className="text-[3.5rem] font-bold">Pomomac</h1>
+      <h2 className="text-[1.5rem] font-semibold">
+        Focus up with Pomomac Timer
+      </h2>
+    </section>
+  );
+}
+
+export default Hero;
