0e726222b689a928ebc826e4313ab3bebc8d3d7b
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-26 00:39:09 +0800

    refactor: Update Header component to use dark mode text color

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { ToggleMode } from "./ToggleMode";
 function Header() {
   return (
     <div className="flex justify-between p-12 lg:p-20 md:p-24 sm:p-24">
-      <h1>Pomomac</h1>
+      <h1 className="dark:text-white text-black font-bold">Pomomac</h1>
       <ToggleMode />
     </div>
   );
