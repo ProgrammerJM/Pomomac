e352ac00486e862d29927cd3f1a232ffb9926042
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-27 06:03:54 +0800

    feat: Update profile page to display user information and add login button
    The profile page has been updated to display the user's information and include a login button for unauthorized users. This change enhances the user experience by providing personalized content and a clear call-to-action for logging in.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
 import React from "react";
+import useAuth from "@/hook/useAuth";
+import { useRouter } from "next/navigation";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Profile() {
-  return <div>Hi, This is your profile</div>;
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return user ? (
+    <div>
+      <Header />
+      <h1>Welcome, {user}!</h1>
+      <Footer />
+    </div>
+  ) : (
+    <div>
+      <h1>Unauthorized</h1>
+      <button onClick={() => router.push("/login")}>Login</button>
+    </div>
+  );
 }
