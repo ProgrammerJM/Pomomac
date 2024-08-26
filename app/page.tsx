0cff41be915efc23602eea1cdb6e81ade7fdc72e
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-27 06:03:03 +0800

    refactor: Remove unnecessary whitespace in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col mx-auto overflow-clip ">
+    <main className="relative flex justify-center items-center flex-col mx-auto overflow-clip">
       <div className="max-w-7xl w-full">
         <Header />
         <Hero />
