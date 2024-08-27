a59cef4bb1d6c876a725bda1ff09f36dbb1af8cd
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-28 03:24:02 +0800

    refactor: Remove setUser state and unnecessary console logs in handleSignIn
    The setUser state and console logs in the handleSignIn function have been removed. This change simplifies the code and removes unnecessary logging statements.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ import React, { SetStateAction, useState } from "react";
 async function handleSignIn(
   event: React.FormEvent<HTMLFormElement>,
   setError: React.Dispatch<SetStateAction<string | null>>,
-  setUser: React.Dispatch<SetStateAction<string | null>>,
   router: ReturnType<typeof useRouter>
 ) {
   event.preventDefault();
@@ -16,47 +15,40 @@ async function handleSignIn(
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("http://localhost:5000/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
+      credentials: "include", // Ensure cookies are included in the request
     });
 
+    const data = await response.json();
+
     if (!response.ok) {
-      const data = await response.json();
-      setError(data.error);
+      return setError(data.error);
     }
 
-    const data = await response.json();
-    const { accessToken, refreshToken, user } = data;
-    console.log("Access Token:", accessToken);
-    console.log("Refresh Token:", refreshToken);
-    console.log("User:", user);
+    const { accessToken, refreshToken, existingUser } = data;
+    console.log("Access token:", accessToken);
+    console.log("Refresh token:", refreshToken);
+    console.log("Existing user:", existingUser);
 
-    // Store the accessToken securely (e.g., in httpOnly cookies or local storage)
-    localStorage.setItem("accessToken", accessToken);
-
-    // Update the user state
-    setUser(user);
-
-    // Redirect to a protected route
-    router.push("/profile");
+    // router.push("/profile");
   } catch (error: any) {
-    console.error(error);
-    setError("An unexpected error occurred.");
+    console.error("Login failed:", error.message);
+    setError("Sign in failed.");
   }
 }
 
 function LoginForm() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
-  const [user, setUser] = useState<string | null>(null);
 
   return (
     <section className="flex flex-col justify-center items-center h-dvh">
       <h1>Pomomac</h1>
       <form
-        onSubmit={(event) => handleSignIn(event, setError, setUser, router)}
+        onSubmit={(event) => handleSignIn(event, setError, router)}
         className="flex flex-col"
       >
         <input name="email" type="email" placeholder="Email" />
