60abd473f62a65ed9973dee3f3298386e80addce
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-29 00:47:11 +0800

    refactor: Add logout functionality to ProfileHeader component
    The ProfileHeader component has been updated to include a logout button. Clicking on the button triggers the logOutUser function, which sends a POST request to the "/api/auth/logout" endpoint to log out the user. This change improves the user experience by providing a convenient way to log out from the profile page.

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { ToggleMode } from "./ToggleMode";
-import { BsThreeDots } from "react-icons/bs";
 import Pomomac from "@/public/pomomaclogo.svg";
 import Image from "next/image";
+import { logOutUser } from "@/app/services/logOutUser";
+import { BiLogOut } from "react-icons/bi";
 
 function ProfileHeader() {
   return (
     <div className="flex justify-between items-center p-9 lg:p-16 md:p-16 sm:p-14">
       <Image src={Pomomac} width={60} height={60} alt="Pomomac Logo" />
       <div className="flex">
+        <div className="flex justify-evenly w-24 mr-12">
+          <button
+            onClick={logOutUser}
+            className="flex justify-center items-center border border-black dark:border-white rounded-lg dark:text-white text-black px-2 py-2"
+          >
+            <BiLogOut
+              className="dark:text-white text-black w-auto mr-2"
+              size={20}
+            />
+            <span className="whitespace-nowrap">Logout</span>
+          </button>
+        </div>
         <ToggleMode />
       </div>
     </div>
