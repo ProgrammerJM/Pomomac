1842a98e1e7d982d24901621c5c3474868065726
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-30 05:37:07 +0800

    refactor: Remove unused image import and update header text
    The ProfileHeader component has been refactored to remove the unused image import for the Pomomac logo. Instead, a simple text element with the title "Pomomac" is now used as the header. This change improves code cleanliness and removes unnecessary dependencies.

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { ToggleMode } from "./ToggleMode";
-import Image from "next/image";
 import { logOutUser } from "@/app/services/logOutUser";
 import { BiLogOut } from "react-icons/bi";
 import UserSettings from "@/components/UserSettings";
+import { useRouter } from "next/navigation";
 
 function ProfileHeader() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    logOutUser(router);
+  };
+
   return (
     <div className="flex justify-between items-center p-9 lg:p-16 md:p-16 sm:p-14">
       <h1 className="dark:text-white text-black font-bold">Pomomac</h1>
       <div className="flex">
         <div className="flex justify-evenly w-24 mr-12">
           <button
-            onClick={logOutUser}
+            onClick={handleLogout}
             className="flex justify-center items-center border border-black dark:border-white rounded-lg dark:text-white text-black px-2 py-2"
           >
             <BiLogOut
