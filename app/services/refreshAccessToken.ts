96cd9d0d32fb0ce57eb46f7e888e74d947979195
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-30 05:38:35 +0800

    refactor: Update refreshAccessToken function to use useRouter hook
    The refreshAccessToken function in the refreshAccessToken.ts file has been updated to include the useRouter hook from the next/navigation package. This change allows for better navigation handling when refreshing the access token, as it now uses the router.push method instead of directly modifying the window location.

--- app/services/refreshAccessToken.ts
+++ app/services/refreshAccessToken.ts
@@ -1,21 +1,30 @@
-async function refreshAccessToken() {
-  const response = await fetch("http://localhost:5000/api/auth/refreshToken", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
+import { useRouter } from "next/navigation";
+async function refreshAccessToken(router: ReturnType<typeof useRouter>) {
+  try {
+    const response = await fetch(
+      "http://localhost:5000/api/auth/refreshToken",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    );
 
-  const data = await response.json();
+    const data = await response.json();
+    console.log(data);
 
-  if (!response.ok) {
-    // Handle refresh token expiry or invalidation
-    window.location.href = "/login";
-    throw new Error(data.error);
+    if (!response.ok) {
+      router.push("/login");
+      throw new Error(data.error || "Failed to refresh token");
+    }
+
+    return { accessToken: data.accessToken };
+  } catch (err: any) {
+    console.error("Error refreshing access token:", err.message);
+    return null; // Return null if there's an error
   }
-
-  return data.accessToken;
 }
 
 export { refreshAccessToken };
