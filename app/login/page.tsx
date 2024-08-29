71168010cd3c0d48242f5c0ea31085d72c041ec9
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-30 05:37:16 +0800

    refactor: Update login page to use handleSignIn function
    The login page has been refactored to use the handleSignIn function from the services/loginUser file. This change improves code organization and separates the login logic into a separate function for better maintainability.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,72 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, { SetStateAction, useState } from "react";
-
-async function handleSignIn(
-  event: React.FormEvent<HTMLFormElement>,
-  setError: React.Dispatch<SetStateAction<string | null>>,
-  setMessage: React.Dispatch<React.SetStateAction<string | null>>,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  router: ReturnType<typeof useRouter>
-) {
-  event.preventDefault();
-  setLoading(true);
-
-  try {
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-
-    const response = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-      credentials: "include", // Ensure cookies are included in the request
-    });
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      setLoading(false);
-      setError(data.error);
-      return;
-    }
-
-    if (response.ok) {
-      setError(null);
-      setLoading(false);
-      setMessage(data.message);
-    }
-
-    router.push("/profile");
-  } catch (error: any) {
-    console.error("Login failed:", error.message);
-    setError("Sign in failed.");
-  }
-}
+import React, { useState } from "react";
+import { handleSignIn } from "../services/loginUser";
 
 function LoginForm() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-
-  if (loading) {
-    return (
-      <h1 className="flex flex-col items-center justify-center h-screen">
-        Loading...
-      </h1>
-    );
-  }
-
-  if (message) {
-    return (
-      <h1 className="flex flex-col items-center justify-center h-screen">
-        {message}
-      </h1>
-    );
-  }
 
   return (
     <section className="flex flex-col justify-center items-center h-dvh">
@@ -89,8 +31,11 @@ function LoginForm() {
           placeholder="Password"
           autoComplete="current-password"
         />
-        <button type="submit">Log In</button>
+        <button type="submit" disabled={loading}>
+          {loading ? "Loading..." : "Log In"}
+        </button>
         {error && <p className="text-red-500">{error}</p>}
+        {message && <p className="text-green-500">{message}</p>}
       </form>
     </section>
   );
