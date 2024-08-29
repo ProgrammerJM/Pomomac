6069d560dd24c9e91a745060c6f51593060298d6
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-30 05:43:25 +0800

    feat: Add login function that handles the logic of signing in to the backend. Detach this logic inside of the login route to seperate the logic from rendering function.

---
+++ app/services/loginUser.ts
@@ -0,0 +1,51 @@
+import { useRouter } from "next/navigation";
+import React, { SetStateAction } from "react";
+
+async function handleSignIn(
+  event: React.FormEvent<HTMLFormElement>,
+  setError: React.Dispatch<SetStateAction<string | null>>,
+  setMessage: React.Dispatch<React.SetStateAction<string | null>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  router: ReturnType<typeof useRouter>
+) {
+  event.preventDefault();
+  setLoading(true);
+
+  try {
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (!email || !password) {
+      setError("All fields are required.");
+      setLoading(false);
+      return;
+    }
+
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+      credentials: "include", // Ensure cookies are included in the request
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setError(data.error);
+      setLoading(false);
+      return;
+    }
+
+    setError(null);
+    setLoading(false);
+    setMessage(data.message);
+    router.push("/profile");
+  } catch (error: any) {
+    console.error("Login failed:", error.message);
+    setError("Sign in failed.");
+    setLoading(false);
+  }
+}
+
+export { handleSignIn };
