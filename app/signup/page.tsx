9f3e6efdfc1cc0e8f94e8aad66cb30426bdf7cbd
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-30 22:38:11 +0800

    feat: Modify the form component to use the component of AceternityUI signup form for a more elegant UI.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,63 +1,12 @@
 "use client";
-
-import Link from "next/link";
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
-import React, { FormEvent, useState } from "react";
-
-async function handleSubmitSignUp(
-  event: FormEvent<HTMLFormElement>,
-  setError: React.Dispatch<React.SetStateAction<string | null>>,
-  setMessage: React.Dispatch<React.SetStateAction<string | null>>,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  router: ReturnType<typeof useRouter>
-) {
-  event.preventDefault();
-  setLoading(true);
-
-  try {
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const confirmPassword = formData.get("confirmPassword") as string;
-
-    if (password !== confirmPassword) {
-      setError("Passwords do not match.");
-      setLoading(false);
-      return;
-    }
-
-    if (!email || !password || !confirmPassword) {
-      setError("All fields are required.");
-      setLoading(false);
-      return;
-    }
-
-    const response = await fetch("http://localhost:5000/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      setError(data.error);
-      setLoading(false);
-      return;
-    }
-
-    if (response.ok) {
-      setError(null);
-      setLoading(false);
-      setMessage(data.message);
-      router.push("/login");
-    }
-  } catch (error: any) {
-    console.error(error);
-  }
-}
+import { handleSubmitSignUp } from "../services/signUpUser";
+import Link from "next/link";
 
 export default function SignUpForm() {
   const [error, setError] = useState<string | null>(null);
@@ -66,45 +15,143 @@ export default function SignUpForm() {
   const router = useRouter();
 
   return (
-    <section className="flex flex-col justify-center items-center h-dvh">
-      <h1>Pomomac</h1>
-      <h2>Create an Account</h2>
+    <div className="max-w-md w-full lg:mt-10 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Welcome to Pomomac
+      </h2>
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        Start your learning journey with Pomomac. Sign up to get started.
+      </p>
       <form
+        className="my-8"
         onSubmit={(event) =>
           handleSubmitSignUp(event, setError, setMessage, setLoading, router)
         }
-        className="flex flex-col"
       >
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          autoComplete="email"
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          autoComplete="current-password"
-        />
-        <input
-          name="confirmPassword"
-          type="password"
-          placeholder="Confirm Password"
-          autoComplete="current-password"
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Loading..." : "Sign Up"}
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstName">First name</Label>
+            <Input
+              id="firstName"
+              placeholder="First Name"
+              type="text"
+              name="firstName"
+              autoComplete="given-name"
+            />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label htmlFor="lastName">Last name</Label>
+            <Input
+              id="lastName"
+              placeholder="Last Name"
+              type="text"
+              name="lastName"
+              autoComplete="family-name"
+            />
+          </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input
+            id="email"
+            placeholder="Email Address"
+            type="email"
+            name="email"
+            autoComplete="email"
+          />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            placeholder="••••••••"
+            type="password"
+            name="password"
+            autoComplete="current-password"
+          />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="confirmPassword">Confirm password</Label>
+          <Input
+            id="confirmPassword"
+            placeholder="••••••••"
+            type="password"
+            name="confirmPassword"
+            autoComplete="current-password"
+          />
+        </LabelInputContainer>
+
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign up \u2192"}
+          <BottomGradient />
         </button>
-        {error && <p className="text-red-500">{error}</p>}
-        {message && <p className="text-green-500">{message}</p>}
+
+        {error && <p className="text-red-500 mt-2 text-center">{error}</p>}
+        {message && (
+          <p className="text-green-500 mt-2 text-center">{message}</p>
+        )}
+
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+        <div className="flex flex-col space-y-4">
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Gmail
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <span className="text-center">
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              className="dark:text-white text-black underline"
+            >
+              Log In
+            </Link>
+          </span>
+        </div>
       </form>
-      <p>
-        Already have an account?{" "}
-        <Link href="/login" className="text-blue-500">
-          Log in
-        </Link>
-      </p>
-    </section>
+    </div>
   );
 }
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
