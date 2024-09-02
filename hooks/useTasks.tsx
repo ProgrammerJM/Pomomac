a0b0c7e37a29d3edfb4daee26240ccf9ff50aace
John Mark Tizado <johnmarktizado@gmail.com>
2024-09-03 02:51:02 +0800

    refactor: Update the Intiial Idea of fetching task from a route that has authentication middleware in backend. This is commit added the useTasks hook to handle token refreshing
    The useTasks hook in the useTasks.tsx file has been updated to handle token refreshing. This change ensures that when the API returns a 401 status code, indicating an expired token, the hook will refresh the access token and retry fetching tasks with the new tokens. This improves the reliability and security of the task management feature.

--- hooks/useTasks.tsx
+++ hooks/useTasks.tsx
@@ -3,6 +3,10 @@
 import { Tasks } from "@/interfaces/Pomodoro";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import { refreshAccessToken } from "@/app/services/protectedRoutes/refreshAccessToken";
+
+let isRefreshing = false;
+let refreshPromise: Promise<any> | null = null;
 
 const useTasks = (refresh: boolean) => {
   const [tasks, setTasks] = useState<Tasks[]>([]);
@@ -10,28 +14,44 @@ const useTasks = (refresh: boolean) => {
   const [error, setError] = useState("");
   const router = useRouter();
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/api/task/todos", {
-          method: "GET",
-          credentials: "include",
-        });
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/task/todos", {
+        method: "GET",
+        credentials: "include",
+      });
 
-        console.log(response);
-        if (!response.ok) {
-          throw new Error("Failed to fetch tasks");
+      if (response.status === 401) {
+        if (!isRefreshing) {
+          isRefreshing = true;
+          refreshPromise = refreshAccessToken(router).finally(() => {
+            isRefreshing = false;
+            refreshPromise = null;
+          });
         }
+
+        await refreshPromise; // Wait for the refresh token process to complete
+
+        // Retry fetching tasks with new tokens
+        return fetchTasks();
+      }
+
+      if (response.ok) {
         const data = await response.json();
         setTasks(data);
-      } catch (error: any) {
-        router.push("/login");
-        setError(error.message);
-      } finally {
-        setLoading(false);
+      } else {
+        throw new Error("Failed to fetch tasks");
       }
-    };
+    } catch (error: any) {
+      console.error(error.message);
+      setError(error.message);
+      router.push("/login");
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchTasks();
   }, [refresh]);
 
